f30e4aabbf112ccf8a953606fa2ccf23dd3d7098
katty <51147935+kattyyyy@users.noreply.github.com>
2021-06-03 13:31:56 +0900

    Add files via upload

---
+++ SigletonTest/SingletonTest.java
@@ -0,0 +1,29 @@
+public class SingletonTest{
+    public static void main(String[] args){
+        Renban renbanA = Renban.getInstance();
+        Renban renbanB = Renban.getInstance();
+        System.out.println( renbanA.getNumber() );
+        System.out.println( renbanB.getNumber() );
+        System.out.println( renbanB.getNumber() );
+        System.out.println( renbanA.getNumber() );
+        System.out.println( renbanB.getNumber() );
+    }
+}
+
+class Renban{
+    private static Renban instance = new Renban();
+    private int number;
+
+    private Renban(){
+        number = 0;
+    }
+    public static Renban getInstance(){
+        return instance;
+    }
+
+    public String getNumber(){
+        number += 1;
+        return String.valueOf(number);
+    }
+
+}
